508c83903e807c3f8a09cf4a8d87fef33df8b484
Mathis Stone <thepsychedelliclucario@Mathiss-MacBook-Pro.local>
2022-10-13 18:09:10 -0400

    js comments

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-;
+//set pool of numbers 1-100
+//generate number to guess to
+//make an array for the 5 guess boxes
+//if user inputs 1-100, compare it to number and put it in next up guess box
+    //if it equals number then tell them they won, then reset the game
+    //else if it doesnt then display message saying its not right, gives them  hint in h3, and move on to the next guess box
+    //else if they get to the last box and get it wrong, tell them they lost in h3
+//else if they input anything else, display error mwssage in h3
